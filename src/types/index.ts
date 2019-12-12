ec3763ca2b5977185911a6fc7e83ce15cc29ccae
chenxiaowen <819339194@qq.com>
2019-12-12 17:52:10 +0800

    feat(update package and add index.ts): update package and add index.ts  types  xhr.ts

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTION'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
